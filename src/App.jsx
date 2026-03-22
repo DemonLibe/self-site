29d89c711d1aa9ff7fb02e51ca239a99f1611060
Аслямов Артур Айратович <AslyamovAA@corp.tatneft.tatar>
2026-03-22 13:05:31 +0300

    added new files to project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Projects from "./components/Projects";
+import About from "./components/About";
+
+import React from 'react'
+
+function App() {
+  
+  
+  
+    return (
+    <div>
+      
+    </div>
+  )
+}
